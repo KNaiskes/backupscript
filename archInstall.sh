32dc5479a91c0c6fb4dbdbb0a7bfa7053c9be40b
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2018-08-25 16:11:31 +0300

    add more programs in the arch list

--- archInstall.sh
+++ archInstall.sh
@@ -2,7 +2,8 @@
 
 # the very basic programs
 declare -a programList
-programList=("vim" "tmux" "git" "xorg-server" "xorg-xinit" "ttf-dejavu")
+programList=("vim" "tmux" "git" "xorg-server" "xorg-xinit" "ttf-dejavu"
+"alsa-utils" "pulseaudio" "i3-wm" "i3bar" "i3stauts" "i3lock")
 
 for i in "${programList[@]}"
 do
