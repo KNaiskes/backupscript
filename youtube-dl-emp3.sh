5df232832368d287fa887ca072e29edcd78a3e12
Kiriakos Naiskes <kiriakosnaiskes@gmail.com>
2019-03-24 00:19:43 +0200

    finished youtube-dl-emp3.sh script - it reads url and download it as emp3

--- youtube-dl-emp3.sh
+++ youtube-dl-emp3.sh
@@ -2,6 +2,7 @@
 
 enviroment_dir="youtube/"
 download_dir="${enviroment_dir}downloads"
+youtube_dl_command="youtube-dl --extract-audio --prefer-ffmpeg --audio-format mp3"
 
 if [ ! -d $download_dir ]; then
     mkdir -p $download_dir
@@ -11,5 +12,12 @@ fi
 
 if [ ! -d "${enviroment_dir}venv" ]; then
     echo "Setting up a Python virtual enviroment and installing youtube_dl"
-    ( cd $enviroment_dir && python -m venv venv && source venv/bin/activate && pip install youtube_dl )
+    ( cd $enviroment_dir && python -m venv venv && source venv/bin/activate && pip install youtube_dl && deactivate )
 fi
+
+read -p "Enter YouTube link: " youtube_link
+download_mp3=("$youtube_dl_command" "$youtube_link")
+
+echo "${download_mp3[@]}"
+
+( cd $enviroment_dir && source venv/bin/activate && cd "../${download_dir}" && eval "${download_mp3[@]}" && deactivate )
